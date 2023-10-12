4e648afae4455080aff01c7db13feef74ae1371d
pistirozsa <pisti.rzsa@gmail.com>
2023-10-12 21:31:22 +0200

    remove bg from pre

--- js/test.js
+++ js/test.js
@@ -1,31 +1,17 @@
-// Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 13);
+document.addEventListener("DOMContentLoaded", function () {
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  var darkLayer = L.tileLayer.dark();
+  var lightLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-// OpenStreetMap layer
-var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
-}).addTo(map);
+  darkLayer.addTo(map); // Default: Dark Mode
 
-// Street layer (Mapbox Streets)
-var streetLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: '© Mapbox',
-  id: 'mapbox/streets-v11',
-  accessToken: 'pk.eyJ1IjoiaXN0dmFucm96c2EiLCJhIjoiY2xubjV3N3hsMDNkZzJqbGpibWdlbGc5ZyJ9.PtRFLZGwtfFTRzAVeB4osg'
+  document.getElementById("toggleMode").addEventListener("click", function () {
+      if (map.hasLayer(darkLayer)) {
+          map.removeLayer(darkLayer);
+          map.addLayer(lightLayer);
+      } else {
+          map.removeLayer(lightLayer);
+          map.addLayer(darkLayer);
+      }
+  });
 });
-
-// Satellite layer (Mapbox Satellite)
-var satelliteLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: '© Mapbox',
-  id: 'mapbox/satellite-v9',
-  accessToken: 'pk.eyJ1IjoiaXN0dmFucm96c2EiLCJhIjoiY2xubjV3N3hsMDNkZzJqbGpibWdlbGc5ZyJ9.PtRFLZGwtfFTRzAVeB4osg'
-});
-
-// Define base maps object to switch between layers
-var baseMaps = {
-  "OpenStreetMap": osmLayer,
-  "Street Map": streetLayer,
-  "Satellite Map": satelliteLayer
-};
-
-// Add layer control to switch between different map types
-L.control.layers(baseMaps).addTo(map);
